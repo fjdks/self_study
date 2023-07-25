a2030f5fab6cc3fdb800122cd9654c02f3966f4d
sk09102 <sk09102@naver.com>
2023-07-25 20:54:01 +0900

    BOJ_1436

---
+++ Algorithm/BOJ/BOJ_1436/Main.java
@@ -0,0 +1,22 @@
+package BOJ_1436;
+
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		int num = 666;
+		int cnt = 1;
+		
+		while(cnt != N) {
+			num++;
+			if(String.valueOf(num).contains("666")) cnt++;
+		}
+		System.out.println(num);
+	}
+
+}
