60ce571e400fd687b5d1f383f7bebafb4bbc6215
sk09102 <sk09102@naver.com>
2025-04-16 14:36:31 +0900

    BOJ_21736_DFS

---
+++ Algorithm/BOJ/BOJ_21736/Main.java
@@ -0,0 +1,56 @@
+package BOJ_21736;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int N, M, cnt;
+	static int[] di = new int[] {-1, 1, 0, 0};
+	static int[] dj = new int[] {0, 0, -1, 1};
+	static char[][] campus;
+	static boolean[][] v;
+	
+	static void dfs(int cur_i, int cur_j) {
+		if(campus[cur_i][cur_j] == 'P') {
+			cnt++;
+		}
+		for (int d = 0; d < 4; d++) {
+			int ni = cur_i + di[d];
+			int nj = cur_j + dj[d];
+			if(0 <= ni && ni < N && 0 <= nj && nj < M && campus[ni][nj] != 'X' && !v[ni][nj]) {
+				v[ni][nj] = true;
+				dfs(ni, nj);
+			}
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		campus = new char[N][M];
+		v = new boolean[N][M];
+		
+		int[] I_pos = new int[2];
+		
+		for (int n = 0; n < N; n++) {
+			String line = br.readLine();
+			for (int m = 0; m < M; m++) {
+				char c = line.charAt(m);
+				if(c == 'I') I_pos = new int[] {n, m};
+				campus[n][m] = c;
+			}
+		}
+		
+		cnt = 0;
+		dfs(I_pos[0], I_pos[1]);
+		System.out.println(cnt == 0 ? "TT" : cnt);
+		
+	}
+
+}
