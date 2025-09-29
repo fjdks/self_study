d56d25f7b50b2c4d887cc621ea50b728100ba4cb
sk09102 <sk09102@naver.com>
2025-09-29 13:42:06 +0900

    [Algo] BOJ_1925_Math_P

--- Algorithm/BOJ/BOJ_1925/Main.java
+++ Algorithm/BOJ/BOJ_1925/Main.java
@@ -1,10 +1,25 @@
 package BOJ_1925;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	
+	static double getLenSquare(int[] p1, int[] p2) {
+		return Math.pow((p2[0] - p1[0]), 2) + Math.pow((p2[1] - p1[1]), 2);
+	}
+	
+	static void check(double A, double B, double max) {
+		if(A + B == max) System.out.println("JikkakTriangle");
+		else if(A + B > max) System.out.println("YeahkakTriangle");
+		else System.out.println("DunkakTriangle");
+	}
+	
+	static void check2(double A1, double A2, double B) {
+		if(A1 + A2 == B) System.out.println("Jikkak2Triangle");
+		else if(A1 + A2 > B) System.out.println("Yeahkak2Triangle");
+		else System.out.println("Dunkak2Triangle");
+	}
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +33,24 @@ public class Main {
 			points[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
+		if((points[0][1] - points[1][1]) * (points[1][0] - points[2][0]) == (points[1][1] - points[2][1]) * (points[0][0] - points[1][0])) System.out.println("X");
+		else {
+			double AA = getLenSquare(points[0], points[1]);
+			double BB = getLenSquare(points[1], points[2]);
+			double CC = getLenSquare(points[2], points[0]);
+			
+			if(AA == BB && BB == CC) System.out.println("JungTriangle");
+			else if(AA == BB || BB == CC || CC == AA) {
+				if(AA == BB) check2(AA, BB, CC);
+				else if(BB == CC) check2(BB, CC, AA);
+				else check2(CC, AA, BB);
+			} else {
+				if(AA >= BB && AA >= CC) check(BB, CC, AA);
+				else if(BB >= CC && BB >= AA) check(CC, AA, BB);
+				else check(AA, BB, CC);
+			}
+			
+		}
 		
 		
 	}
