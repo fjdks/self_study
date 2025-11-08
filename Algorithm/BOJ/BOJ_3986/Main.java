e3467ef3105b858cf358168d3043597c32446d5b
sk09102 <sk09102@naver.com>
2025-11-08 15:14:23 +0900

    [Algo] BOJ_3986_Stack_P

--- Algorithm/BOJ/BOJ_3986/Main.java
+++ Algorithm/BOJ/BOJ_3986/Main.java
@@ -1,47 +1,33 @@
 package BOJ_3986;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	static int cnt;
-	
-	static void check(String input) {
+	static boolean check(String input) {
+		Stack<Character> s = new Stack<>();
 		
-		int len = input.length();
-		if(len % 2 == 1) return;
-		boolean flag = false;
-		while(true) {
-			if(len == 0 || flag) break;
-			len = input.length();
-//			System.out.println(input);
-			for (int i = 0; i < len - 1; i++) {
-				if(input.charAt(i) == input.charAt(i + 1)) {
-					if(len == 2) {
-						input = "";
-						cnt++;
-					}
-					else {
-//						System.out.println(i);
-						input = input.substring(0, i) + input.substring(i + 2, len);
-//						System.out.println(input);
-						break;
-					}
-				}
-				if(i >= len - 2) flag = true;
+		for (int i = 0; i < input.length(); i++) {
+			if(s.isEmpty()) s.add(input.charAt(i));
+			else {
+				if(s.peek() == input.charAt(i)) s.pop();
+				else s.add(input.charAt(i));
 			}
 		}
+		
+		return s.isEmpty() ? true : false;
+		
 	}
 	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		int N = Integer.parseInt(br.readLine());
-		cnt = 0;
+		int cnt = 0;
 		for (int i = 0; i < N; i++) {
 			String input = br.readLine();
-			check(input);
+			if(check(input)) cnt++;
 		}
 		System.out.println(cnt);
 	}
